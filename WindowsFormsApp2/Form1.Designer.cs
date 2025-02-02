2592b555cdfb6217dfced449cc23556fee333c86
Igor4358 <igor4358@yandex.ru>
2025-02-02 20:16:44 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtChange = new System.Windows.Forms.Button();
+            this.BtClose = new System.Windows.Forms.Button();
+            this.txtWindowTitle = new System.Windows.Forms.TextBox();
+            this.txtNewTitle = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtChange
+            // 
+            this.BtChange.Location = new System.Drawing.Point(53, 201);
+            this.BtChange.Name = "BtChange";
+            this.BtChange.Size = new System.Drawing.Size(75, 23);
+            this.BtChange.TabIndex = 0;
+            this.BtChange.Text = "Change";
+            this.BtChange.UseVisualStyleBackColor = true;
+            this.BtChange.Click += new System.EventHandler(this.BtChange_Click);
+            // 
+            // BtClose
+            // 
+            this.BtClose.Location = new System.Drawing.Point(204, 201);
+            this.BtClose.Name = "BtClose";
+            this.BtClose.Size = new System.Drawing.Size(75, 23);
+            this.BtClose.TabIndex = 1;
+            this.BtClose.Text = "Close";
+            this.BtClose.UseVisualStyleBackColor = true;
+            this.BtClose.Click += new System.EventHandler(this.BtClose_Click);
+            // 
+            // txtWindowTitle
+            // 
+            this.txtWindowTitle.Location = new System.Drawing.Point(53, 76);
+            this.txtWindowTitle.Name = "txtWindowTitle";
+            this.txtWindowTitle.Size = new System.Drawing.Size(100, 22);
+            this.txtWindowTitle.TabIndex = 2;
+            // 
+            // txtNewTitle
+            // 
+            this.txtNewTitle.Location = new System.Drawing.Point(204, 76);
+            this.txtNewTitle.Name = "txtNewTitle";
+            this.txtNewTitle.Size = new System.Drawing.Size(100, 22);
+            this.txtNewTitle.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNewTitle);
+            this.Controls.Add(this.txtWindowTitle);
+            this.Controls.Add(this.BtClose);
+            this.Controls.Add(this.BtChange);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtChange;
+        private System.Windows.Forms.Button BtClose;
+        private System.Windows.Forms.TextBox txtWindowTitle;
+        private System.Windows.Forms.TextBox txtNewTitle;
+    }
+}
+
---
+++ WindowsFormsApp2/Sp - DZ2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        // Константы для сообщений
+        private const uint WM_SETTEXT = 0x000C;
+        private const uint WM_CLOSE = 0x0010;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtChange_Click(object sender, EventArgs e)
+        {
+            string windowTitle = txtWindowTitle.Text;
+            string newTitle = txtNewTitle.Text;
+
+            // Поиск окна по заголовку
+            IntPtr hWnd = FindWindow(null, windowTitle);
+
+            if (hWnd != IntPtr.Zero)
+            {
+                // Отправка сообщения для изменения заголовка
+                SendMessage(hWnd, WM_SETTEXT, IntPtr.Zero, Marshal.StringToHGlobalUni(newTitle));
+                MessageBox.Show("Заголовок окна изменен.");
+            }
+            else
+            {
+                MessageBox.Show("Окно не найдено.");
+            }
+        }
+
+        private void BtClose_Click(object sender, EventArgs e)
+        {
+            string windowTitle = txtWindowTitle.Text;
+
+            // Поиск окна по заголовку
+            IntPtr hWnd = FindWindow(null, windowTitle);
+
+            if (hWnd != IntPtr.Zero)
+            {
+                // Отправка сообщения для закрытия окна
+                SendMessage(hWnd, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+                MessageBox.Show("Окно закрыто.");
+            }
+            else
+            {
+                MessageBox.Show("Окно не найдено.");
+            }
+        }
+    }
+
+    }
